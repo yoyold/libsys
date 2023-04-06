b6afdabbb001a58507d4281e6e525079777b599b
Lukas D <ldony@mail.de>
2023-04-06 13:09:20 -0700

    Add simple Exception logging

--- src/Exceptionlog.cpp
+++ src/Exceptionlog.cpp
@@ -4,13 +4,35 @@
 #pragma hdrstop
 
 #include "Exceptionlog.h"
+
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TExceptionLogFrm *ExceptionLogFrm;
 //---------------------------------------------------------------------------
 __fastcall TExceptionLogFrm::TExceptionLogFrm(TComponent* Owner)
-    : TForm(Owner)
+	: TForm(Owner)
 {
+	displayLogFile("logfile.txt");
 }
+
+void TExceptionLogFrm::displayLogFile(const std::string& filename)
+{
+	ExceptionLogMemo->Lines->LoadFromFile(filename.c_str());
+}
+
 //---------------------------------------------------------------------------
+ExceptionLog::ExceptionLog(const std::string& filename)
+	: m_logFile(filename, std::ios::app)
+{
+	if (!m_logFile.is_open()) {
+		throw std::runtime_error("Could not open log file");
+    }
+}
+
+void ExceptionLog::logException(const std::exception& e)
+{
+    m_logFile << "Exception caught: " << e.what() << std::endl;
+}
+
+
--- src/Exceptionlog.h
+++ src/Exceptionlog.h
@@ -7,15 +7,29 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <stdexcept>
+#include <fstream>
+#include <string>
 //---------------------------------------------------------------------------
 class TExceptionLogFrm : public TForm
 {
 __published:	// IDE-managed Components
     TMemo *ExceptionLogMemo;
 private:	// User declarations
+	void displayLogFile(const std::string& filename);
 public:		// User declarations
     __fastcall TExceptionLogFrm(TComponent* Owner);
 };
+
+class ExceptionLog {
+public:
+    ExceptionLog(const std::string& filename);
+    void logException(const std::exception& e);
+private:
+	std::ofstream m_logFile;
+};
+
+
 //---------------------------------------------------------------------------
 extern PACKAGE TExceptionLogFrm *ExceptionLogFrm;
 //---------------------------------------------------------------------------
